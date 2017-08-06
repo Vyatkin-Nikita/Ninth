d3ce02e981c692b50f593b9b544a1a3e898f317a
Никита Вяткин <vyatkin-nikita@mail.ru>
2017-08-06 18:52:29 +0500

    Методы создания и удаление элементов двунаправленного списка

---
+++ Ninth Task/Ninth Task/BinList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninth_Task
+{
+    class BinList//Класс для двунаправленных списков
+    {
+
+        private int data;//иформационное поле элемента списка (строка)
+        public BinList next;//адресное поле элемента списка (указывает на следующий элемент)
+        public BinList prev;//адресное поле элемента списка (указывает на предыдущий элемент)
+        public int Data
+        {
+            get { return data; }
+        }
+        public BinList Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+        public BinList Prev
+        {
+            get { return prev; }
+            set { prev = value; }
+        }
+        public BinList()
+        {
+            data = 0;
+            next = null;
+            prev = null;
+        }//Создание пустого элемента  списка
+        public BinList(int d)
+        {
+            data = d;
+            next = null;
+            prev = null;
+        }//Создание элемента списка       
+
+        public static BinList Add(int size)
+        {
+
+            int info = 1;
+            BinList beg = new BinList(info);
+
+            BinList temp = beg;
+            for (int i = 1; i < size; i++)
+            {
+                info++;
+                BinList NewElem = new BinList(info);
+                temp.Next = NewElem;
+                NewElem.Prev = temp;
+                temp = NewElem;
+            }
+            return beg;
+        }//Создание списка добавлением элементов в конец списка  
+        public static BinList Ydalenie(BinList beg, int number)
+        {
+            if (beg == null)
+            {
+                Console.WriteLine("Список пуст");
+                return null;
+            }
+            if (number == 1)
+            {
+                beg = beg.next;
+                return beg;
+            }
+            BinList p = beg;
+            for (int i = 1; i < number - 1 && p != null; i++)
+                p = p.next;
+            if (p == null)
+            {
+                Console.WriteLine("Размер списка меньше введённого числа");
+                return beg;
+            }
+            p.next = p.next.next;
+            p.next.next.prev = p;
+            return beg;
+        }//Удаление элемента списка с указанным номером
+        public static void ShowList(BinList beg)
+        {
+            if (beg == null)
+            {
+                Console.WriteLine("Список пуст");
+                return;
+            }
+            BinList p = beg;
+            while (p != null)
+            {
+                Console.Write(p + " ");
+                p = p.next;
+
+            }
+            Console.WriteLine();
+        }//Показать список
+        public override string ToString()
+        {
+            return data + " ";
+        }
+    }
+}
---
+++ Ninth Task/Ninth Task/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninth_Task
+{
+    class Program
+    {
+        static BinList Beg;//Первый элемент двунаправленного списка
+        static int VvodProverka(int mogr = -1, int bogr = -1)//Проверка вводимых с клавиатуры чисел, mogr и bogr - минимально и максимально возможные значения числа
+        {
+            bool ok;
+            int n;
+            do
+            {
+                ok = int.TryParse(Console.ReadLine(), out n);
+                if (!ok) Console.WriteLine("Ошибка. Неверный формат данных. Повторите ввод.");
+                if (((n <= mogr) && (mogr != -1) || ((n > bogr) && (bogr != -1))) && (ok)) { Console.WriteLine("Ошибка. Число должно находится в пределах от {0} до {1} . Повторите ввод.", mogr + 1, bogr); ok = false; }
+
+            } while (!ok);
+            Console.WriteLine();
+            return n;
+        }
+        static void Menu()//Меню задачи
+        {
+
+            Console.WriteLine("Выберите действие:\n1. Создать новый двунаправленный список\n2. Найти элемент списка\n3. Удалить элемент списка\n4. Выход из программы");
+            int i = VvodProverka(0, 4);
+            switch (i)
+            {
+                case 1:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Введите число N");
+                        int N = VvodProverka(0);
+                        Beg = BinList.Add(N);
+                        BinList.ShowList(Beg);
+                        Console.WriteLine();
+                        Menu();
+                    }
+                    break;
+                case 2:
+                    {
+                        if (Beg == null) { Console.Clear(); Console.WriteLine("Список пуст."); Menu(); }
+                        Console.WriteLine("Введите номер добавляемой строки");
+                        int s = VvodProverka(0, 4);
+                        if (Beg == null && s != 1)
+                        { Console.WriteLine("Ошибка! Длина списка меньше введённого номера"); Menu(); }
+                        Console.Clear();
+                        BinList.ShowList(Beg);
+                      
+                        BinList.ShowList(Beg);
+                        
+                        Console.WriteLine(); Menu();
+                    }
+                    break;
+                case 3:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Введите информационное поле удаляемого элемента");
+                        int info = VvodProverka(0);
+                        Beg = null;
+                        Console.WriteLine(); Menu();
+                    }
+                    break;
+                case 4:
+                    Environment.Exit(0);
+                    break;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+    }
+}
